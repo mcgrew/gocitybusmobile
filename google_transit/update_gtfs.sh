7024ab0a25a3ff6ba98507009cbd85ec141aec9d
Thomas McGrew <tjmcgrew@gmail.com>
2011-08-22 11:35:14 -0400

    Updated google transit feeds

--- google_transit/update_gtfs.sh
+++ google_transit/update_gtfs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl http://gocitybus.com/GTFS/google_transit.zip > google_transit.zip
+wget http://gocitybus.com/GTFS/google_transit.zip
 unzip -o google_transit.zip
-rm google_transit.zip
+rm -v google_transit.zip
 
